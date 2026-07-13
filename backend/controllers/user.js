9b084ad7cc1827926f75e9761061f534e9863449
RAVI TEJA <narniraviteja@gmail.com>
2026-07-13 20:30:14 +0530

    Day 2 completed

---
+++ backend/controllers/user.js
@@ -0,0 +1,43 @@
+const bcrypt = require("bcryptjs");
+const User = require("../models/user");
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "Please fill all fields" });
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+const loginUser = async (req, res) => {
+  res.json({
+    message: "Login User API",
+  });
+};
+
+module.exports = {
+  registerUser,
+  loginUser,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/user");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("AI Code Review Assistant Backend Running");
 });
 
 const PORT = process.env.PORT || 5000;
-
+connectDB();
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
